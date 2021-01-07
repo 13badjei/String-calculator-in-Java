a8d57ea96d730a7cf7c5c99b4a153e259074dd1e
13badjei <13badjei@gmail.com>
2021-01-07 14:04:29 +0000

    Add files via upload

---
+++ StringCalculator/src/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String numbers = "//[***][%]\n1***2%3";
+
+        StringCalculator calculator = new StringCalculator();
+        int output = calculator.add(numbers);
+        System.out.println(" output = " + output);
+
+    }
+}
